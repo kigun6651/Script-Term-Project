2227ec8aa2a2786869092bed3d650918614b97b9
kigun6651 <kigun6651@gmail.com>
2023-05-24 16:52:38 +0900

    tab_menu 추가 검색, 즐겨찾기, 지도 탭을 추가했다.

--- MainGUI.py
+++ MainGUI.py
@@ -1,12 +1,28 @@
 from tkinter import *
+from tkinter import ttk
 
 
 class MainGUI:
     def __init__(self):
+        # 윈도우 생성
         self.window = Tk()
         self.window.title('도서 탐색기')
-        self.window.geometry('800x600')
-        self.window.configure(bg='white')
+
+       # 탭 메뉴
+        tab_menu = ttk.Notebook(self.window, width=800, height=600)
+        tab_menu.pack()
+
+        # 검색 탭
+        search_tab = Frame(self.window)
+        tab_menu.add(search_tab, text="Search")
+
+        # 즐겨찾기 탭
+        favorite_tab = Frame(self.window)
+        tab_menu.add(favorite_tab, text="Favorite")
+
+        # 지도 탭
+        map_tab = Frame(self.window)
+        tab_menu.add(map_tab, text="Map")
 
         self.window.mainloop()
 
